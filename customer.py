63bc14ceffe72f3946afd516ff860c4cfc25f9df
Your Name <you@example.com>
2019-04-30 18:36:22 +0400

    customer.py

---
+++ customer.py
@@ -0,0 +1,11 @@
+# line = (line[1:].split(","))
+def check_balance(customer_code):    
+        with open('customers.csv', 'r') as f:
+            customers = f.readlines()   
+            customers.pop(0) 
+            
+            for i in customers:
+                customer = i.split(',')
+                if customer_code == customer[0]:
+                    return customer[3]
+
